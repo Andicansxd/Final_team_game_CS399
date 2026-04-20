d8fbea60a6e55a5ec4d8a611a701be0e1d751490
Lukie4reals <lajoplin@protonmail.com>
2026-04-20 10:11:53 -0500

    changes

--- battle_system/team_controllers/player_controller.gd
+++ battle_system/team_controllers/player_controller.gd
@@ -18,8 +18,6 @@ func _on_hero_moved(move_index: int, battler_index: int, target_battler_array: A
 	print(battler_index, " has been called to move!")
 	print(move_index, " has been called!")
 	var battler: Battler = battler_array[battler_index]
-	print(battler_array[battler_index].move_array)
-	print(battler.move_array)
 	battler.select_move(move_index, target_battler_array)
 	battler_selected_move.emit()
 	print("Battler move decided: ", battler_index)
